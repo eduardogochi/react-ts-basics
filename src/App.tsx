dd2922d91dc1a565742acacd9a1cb6247ef4e923
Eduardo Gochi <eduardo.gochi@gmail.com>
2024-01-23 14:35:36 -0600

    Added CourseGoal and CourseGoalList components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
-import CourseGoal from './components/CourseGoal';
-import Header from './components/Header';
+import { useState } from 'react'
+import Header from './components/Header'
 import goalsImg from './assets/goals.jpg'
+import CourseGoalList from './components/CourseGoalList'
 
-export default function App(){
+export type CourseGoal = {
+  title: string
+  description: string
+  id: number
+}
+
+export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([])
+
+  function handleAddGoal() {
+    setGoals(prevGoals => {
+      const newGoal: CourseGoal = {
+        title: 'Learn react + TS',
+        description: 'Learn in depth',
+        id: Math.random()
+      }
+      return [...prevGoals, newGoal]
+    })
+  }
+
+  function handleDeleteGoal(id: number) {
+    setGoals(prevGoals => prevGoals.filter((goal) => goal.id !== id))
+  }
+
   return (
     <main>
-      <Header image={{ src: goalsImg, alt: 'A list of goals'}}>
+      <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
         <h1>Your course goals</h1>
       </Header>
-      <CourseGoal title="Learning React + TS">
-        <p>From the ground</p>
-        </CourseGoal>
+      <button onClick={handleAddGoal}>Add Goal</button>
+      <CourseGoalList goals={goals} onDeleteGoal={handleDeleteGoal}/>
     </main>
   )
 }
