9f414e0a01a9f550ee6e669383a074172db95602
Eduardo Gochi <eduardo.gochi@gmail.com>
2024-01-22 18:00:36 -0600

    Created CourseGoals and Header components

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import { PropsWithChildren } from 'react'
+
+type HeaderProps = PropsWithChildren<{image: {
+    src: string;
+    alt: string;
+}}>
+
+export default function Header({image, children}: HeaderProps){
+    return (
+        <header>
+            <img {...image} />
+            {children}
+        </header>
+    )
+}
